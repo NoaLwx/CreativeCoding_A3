c596b2b36389d431914b75195217a00187b6dc0d
NoaLwx <130018250+NoaLwx@users.noreply.github.com>
2024-06-05 02:02:39 +1000

    aaaaaaaa

--- public/script.js
+++ public/script.js
@@ -1,9 +1,13 @@
+const socket = new WebSocket (`ws://localhost/`)
+socket.onopen = () => console.log (`client websocket opened!`)
 document.body.style.margin   = 0
 document.body.style.overflow = `hidden`
 
-const cnv = document.querySelector("#canvas");
-// cnv.width = innerWidth
-// cnv.height = 800
+//canvas1---------------------------------------
+
+const cnv = document.querySelector("#box1");
+cnv.width = innerWidth/2;
+cnv.height = innerHeight/3;
 
 const ctx = cnv.getContext (`2d`);
 
@@ -15,6 +19,7 @@ let currentX = cnv.width/2;
 let currentY = cnv.height/2; 
 
 const uploader = document.querySelector("#uploader");
+const inputBox = document.querySelector(".form-group");
 
 
 uploader.addEventListener('change',(e) => {
@@ -24,6 +29,7 @@ uploader.addEventListener('change',(e) => {
 
    img = new Image();
    img.src = URL.createObjectURL(myFile);
+   
    const resetCanvas = () =>{
       ctx.fillStyle = `white`
       ctx.fillRect (0, 0, innerWidth, innerHeight)
@@ -35,17 +41,29 @@ uploader.addEventListener('change',(e) => {
          // img.width = 200;
          // img.height = 200;
 
-         ctx.drawImage(img, currentX - img.width/10, currentY - img.height/10, img.width/2,img.height/2);
+         ctx.drawImage(img, currentX - img.width/6, currentY - img.height/6, img.width/3, img.height/3);
       }, 200);
 
-   };
+   };   
+
+   setTimeout(() => {
+      inputBox.classList.toggle("hide");
+  }, 100);
+
  });
+
+cnv.addEventListener("dblclick", toggleInputVisibility)
+
+ function toggleInputVisibility() {
+   inputBox.classList.toggle("hide");
+ }
 
 //detech if the click is on the image
 cnv.onmousedown = (e) =>{
+
    if (img!== null &&
-      e.layerX < (currentX + cnv.width/2) &&
-      e.layerX > (currentX - cnv.width/2) &&
+      e.layerX < (currentX + img.width/2) &&
+      e.layerX > (currentX - img.width/2) &&
       e.layerY < (currentY + img.height/2) &&
       e.layerY > (currentY - img.height/2))
       {
@@ -72,21 +90,95 @@ cnv.onmouseout = (e) =>{
 }
 });
 
-
-// cnv.onmousedown(){
-
-// }
-
-// const draw_frame = () => {
-//    ctx.fillStyle = `pink`
-//    ctx.fillRect (0, 0, innerWidth, innerHeight)
-
-//    requestAnimationFrame (draw_frame)
-// }
-
-// draw_frame ()
-
 window.onresize = () => {
-   cnv.width = 300
-   cnv.height = 300   
+   cnv.width = innerWidth/2
+   cnv.height = innerHeight/3
+   cnv2.width = innerWidth/2
+   cnv2.height = innerHeight/3
 }
+
+
+//canvas2----------------------------------------
+
+const cnv2 = document.querySelector("#box2");
+cnv2.width = innerWidth/2;
+cnv2.height = innerHeight/3;
+
+const ctx2 = cnv2.getContext (`2d`);
+
+document.addEventListener('DOMContentLoaded', function() {
+
+let img2 = null; 
+let draggable = false;
+let current2X = cnv2.width/2; 
+let current2Y = cnv2.height/2; 
+
+const uploader2 = document.querySelector("#uploader2");
+const inputBox2 = document.querySelector(".form-group2");
+
+
+uploader2.addEventListener('change',(e) => {
+   console.log('upload');
+   const myFile2 = uploader2.files[0];
+   console.log(myFile2.name);
+
+   img2 = new Image();
+   img2.src = URL.createObjectURL(myFile2);
+   
+   const resetCanvas = () =>{
+      ctx2.fillStyle = `white`
+      ctx2.fillRect (0, 0, innerWidth, innerHeight)
+   }
+  
+   img2.onload = () => {
+      setInterval(()=>{
+         resetCanvas();
+
+         ctx2.drawImage(img2, current2X - img2.width/6, current2Y - img2.height/6, img2.width/3, img2.height/3);
+      }, 200);
+
+   };   
+
+   setTimeout(() => {
+      inputBox2.classList.toggle("hide");
+  }, 100);
+
+ });
+
+cnv2.addEventListener("dblclick", toggleInputVisibility)
+
+ function toggleInputVisibility() {
+   inputBox2.classList.toggle("hide");
+ }
+
+//detech if the click is on the image
+cnv2.onmousedown = (e) =>{
+
+   if (img2!== null &&
+      e.layerX < (current2X + img2.width) &&
+      e.layerX > (current2X - img2.width) &&
+      e.layerY < (current2Y + img2.height/2) &&
+      e.layerY > (current2Y - img2.height/2))
+      {
+         draggable = true;
+         console.log("clicked");
+      }
+   else {
+      console.log("nah");
+   }
+}
+
+cnv2.onmousemove = (e) =>{
+   if(draggable){
+      current2X = e.layerX;
+      current2Y = e.layerY;
+  }
+}
+cnv2.onmouseup = (e) =>{
+   draggable = false;
+}
+
+cnv2.onmouseout = (e) =>{
+   draggable = false;
+}
+});
--- server.js
+++ server.js
@@ -4,9 +4,26 @@ import { serveDir } from "https://deno.land/std@0.157.0/http/file_server.ts"
 serve (handler, { port: 80 })
 
 function handler (incoming_req) {
+
+    // console.log (incoming_req)
+
     let req = incoming_req
 
+    const upgrade = req.headers.get ("upgrade") || ""
+
     // if the requested url does not specify a filename
+
+    if (upgrade.toLowerCase() == "websocket") {
+        const { socket, response } = Deno.upgradeWebSocket (req)
+        // return response
+        socket.onopen  = () => {
+            console.log (`server WebSocket opened!`)
+    
+            // sending a message to the client
+            socket.send (`hello from server.js!`)
+    }
+}
+
     if (req.url.endsWith (`/`)) {
 
         // add 'index.html' to the url
