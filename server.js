dfd0b7a7d8e19973d6df4036f81650329d3c80de
NoaLwx <130018250+NoaLwx@users.noreply.github.com>
2024-06-14 00:27:25 +1000

    ssssss

--- server.js
+++ server.js
@@ -2,8 +2,8 @@ import { serve } from "https://deno.land/std@0.158.0/http/server.ts"
 import { serveDir } from "https://deno.land/std@0.158.0/http/file_server.ts"
 import { getNetworkAddr } from "https://deno.land/x/local_ip/mod.ts"
 
-import { open } from "https://deno.land/std/fs/mod.ts";
-const kv = await open("./my-kv-store.kv");
+// import { open } from "https://deno.land/std/fs/mod.ts";
+// const kv = await open("./my-kv-store.kv");
 
 
 const local_ip = await getNetworkAddr()
