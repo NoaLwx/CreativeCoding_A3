56bcb24156be03994c2ed17acbd7c541bf076d46
NoaLwx <130018250+NoaLwx@users.noreply.github.com>
2024-06-21 01:57:22 +1000

    aaa

--- server.js
+++ server.js
@@ -60,7 +60,7 @@ async function handler (incoming_req) {
             console.log (e.data);
             kv.set ([`canvasData`], e.data);        
             sockets.forEach (s => s.send (e.data))
-            const entry = await kv.get ([`canvasData`], e.data);
+            const entry = await kv.get ([`canvasData`]);
 
             channel.postMessage (JSON.stringify(entry));
     }
